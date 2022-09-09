919f9ad419e59bc93bf0d026356216dd4609d3b4
何方 <he529564582@163.com>
2022-09-09 23:56:43 +0800

    add 06

---
+++ 06-setup的2个注意点/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <demoTest msg="hello" @hello="sayHello">
+    <template v-slot:test1>
+      <span>abc1</span>
+    </template>
+    <template v-slot:test2>
+      <span>abc2</span>
+    </template>
+  </demoTest>
+</template>
+
+<script>
+import demoTest from "./components/demoTest"
+export default {
+  name: "App",
+  components: { demoTest },
+  setup() {
+    function sayHello(value) {
+      alert(value)
+    }
+    return {
+      sayHello
+    }
+  }
+}
+</script>
---
+++ 06-setup的2个注意点/components/demoTest.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>个人信息</h1>
+  <h2>姓名:{{ person.name }}</h2>
+  <h2>年龄:{{ person.age }}</h2>
+  <h2>{{ msg }}</h2>
+  <button @click="say">say</button>
+  <slot name="test1"></slot>
+  <slot name="test2"></slot>
+</template>
+
+<script>
+import { reactive } from "vue"
+export default {
+  name: "demoTest",
+  props: ["msg"],
+  emits: ["hello"],
+  // beforeCreate() {
+  //   console.log("beforeCreate")
+  // },
+  // mounted() {
+  //   console.log(this)
+  // },
+  setup(props, context) {
+    // console.log(props)
+    // console.log(context.attrs.msg)
+    // console.log(context.emit)
+    console.log(context.slots)
+
+    // console.log("setup", this)
+    let person = reactive({
+      name: "John",
+      age: 18
+    })
+    function say() {
+      context.emit("hello", 666)
+    }
+    return {
+      person,
+      say
+    }
+  }
+}
+</script>
