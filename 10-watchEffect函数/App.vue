b5a27b90873dc8c8f2826e385e4a666a5fbd6afa
何方 <he529564582@163.com>
2022-09-10 17:35:45 +0800

    更新08,add10,11

---
+++ 10-watchEffect函数/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <demoTest></demoTest>
+</template>
+
+<script>
+import demoTest from "./components/demoTest"
+export default {
+  name: "App",
+  components: { demoTest },
+  setup() {
+    return {}
+  }
+}
+</script>
---
+++ 11-vu3生命周期钩子/components/demoTest.vue
@@ -0,0 +1,43 @@
+<template>
+  <h2>求和：{{ sum }}</h2>
+  <button @click="sum++">sum++</button>
+</template>
+
+<script>
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref
+} from "vue"
+export default {
+  name: "demoTest",
+  setup() {
+    let sum = ref(0)
+    onBeforeMount(() => {
+      console.log("---onBeforeMount---")
+    })
+    onMounted(() => {
+      console.log("---onMounted---")
+    })
+    onBeforeUpdate(() => {
+      console.log("---onBeforeUpdate---")
+    })
+    onUpdated(() => {
+      console.log("---onUpdated---")
+    })
+    onBeforeUnmount(() => {
+      console.log("---onBeforeUnmount---")
+    })
+    onUnmounted(() => {
+      console.log("---onUnmounted---")
+    })
+    return {
+      sum
+    }
+  }
+}
+</script>
