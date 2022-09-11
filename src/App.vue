ecf8efc110847eba37a878261fc143c720b6cf76
何方 <he529564582@163.com>
2022-09-11 18:48:32 +0800

    other

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <demoTest />
+  <div class="app">
+    <h2>我是App组件:{{ name }}---{{ price }}</h2>
+    <Child />
+  </div>
 </template>
-
 <script>
-import demoTest from "./components/demoTest"
+import { provide, reactive, toRefs } from "vue"
+
 export default {
   name: "App",
-  components: { demoTest }
+
+  setup() {
+    let car = reactive({
+      name: "BWM",
+      price: 40
+    })
+    provide("car", car)
+    return {
+      ...toRefs(car)
+    }
+  }
 }
 </script>
+
+<style>
+.app {
+  background: gray;
+  padding: 20px;
+}
+</style>
